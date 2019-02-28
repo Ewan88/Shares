302745d2f90a2affdbd77b95a68fa476a509c732
Ewan88 <baileyewan@gmail.com>
2019-02-28 20:26:30 +0000

    implementing search box...

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,30 @@
 <template lang="html">
-
+  <div id="search-box">
+    <form/>
+  </div>
 </template>
 
 <script>
+import SearchBox from "./components/SearchBox"
 export default {
+  name: 'app',
+  data() {
+    return {
+      boookings: []
+    }
+  }
+},
+components: {
+  SearchBox,
+},
+mounted(){
+  this.fetchPersonal_Stocks();
+
+  eventBus.$on('refresh-data', this.fetchPersonal_Stocks);
+},
+methods: {
+  fetchPersonal_Stocks(){
+    
+  }
 }
 </script>
