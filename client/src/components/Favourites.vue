2c5b83602870be39f98d5c9e161d059d35c3b990
ianbone1 <ianbone1@googlemail.com>
2019-03-01 23:47:36 +0000

    Display tick box change now recorded in DB and Local object, and the wheels on the bus started

--- client/src/components/Favourites.vue
+++ client/src/components/Favourites.vue
@@ -9,7 +9,7 @@
         <th>Qty</th>
       </tr>
       <tr v-for='fav in favourites'>
-        <td><input class="checkbox" type="checkbox" :id="fav._id" :name="fav._id" :checked="fav.display"></input></td>
+        <td><input v-on:change="updateDisplay(fav)" class="checkbox" type="checkbox" :id="fav._id" :name="fav._id" :checked="fav.display"></input></td>
         <td>{{fav.symbol}}</td>
         <td>{{fav.name}}</td>
         <td>{{fav.purchase_date}}</td>
@@ -22,6 +22,7 @@
 
 <script>
 
+import { eventBus } from '../main.js';
 
 export default {
   name: 'Favourites',
@@ -37,10 +38,31 @@ export default {
       fetch('http://localhost:3000/api/shares')
       .then(res => res.json())
       .then(favs => this.favourites=favs)
+    },
+    updateDisplay(element){
+      //update the database
+      let updatedRecord={
+        "symbol": element.symbol,
+        "name":element.name,
+        "qty":element.qty,
+        "display": !element.display,
+        "purchase_date": element.purchase_date
+      };
+      fetch('http://localhost:3000/api/shares/'+ element._id, {
+        method: 'PUT',
+        body: JSON.stringify(updatedRecord),
+        headers: { 'Content-Type': 'application/json'}})
+      //update the local favourites object
+      let index = this.favourites.findIndex(f => f._id==element._id)
+      this.favourites[index].display=!element.display;
+      //fire the event that something has changed.
+      eventBus.$emit("favourites-changes", this.favourites);
     }
   },
   mounted(){
     this.fetchFavourites();
+    // this might crash, may need to check this.favourites is populated
+    eventBus.$emit("favourites-changes", this.favourites);
   },
   components: {
   }
