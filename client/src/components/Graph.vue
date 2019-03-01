65aa05677b8194e0cde504d1c9388a6f02610aef
John Page <jdehpage@gmail.com>
2019-03-01 16:34:23 +0000

    Data filter and restructure function added

--- client/src/components/Graph.vue
+++ client/src/components/Graph.vue
@@ -1,22 +1,22 @@
 <template>
   <div id="graph" >
 
-      <h2 id="heading">Share Tracker</h2>
-      <div class="graphSelectorWrapper">
-        <label for="chartType">Change Graph Type:</label>
-        <select name="chartType" id="chartType" v-model="selectedChartType">
-          <option v-for="chartType in chartTypes" :value="chartType">{{ chartType }}</option>
-        </select>
-      </div>
-      <div v-if="chartData!=[]" id="theChart">
-        <gchart
-        v-if="chartData"
-        :type="selectedChartType"
-        :data="chartData"
-        :options="chartOptions"
-        />
-      </div>
+    <h2 id="heading">Share Tracker</h2>
+    <div class="graphSelectorWrapper">
+      <label for="chartType">Change Graph Type:</label>
+      <select name="chartType" id="chartType" v-model="selectedChartType">
+        <option v-for="chartType in chartTypes" :value="chartType">{{ chartType }}</option>
+      </select>
     </div>
+    <div v-if="chartData!=[]" id="theChart">
+      <gchart
+      v-if="chartData"
+      :type="selectedChartType"
+      :data="chartData"
+      :options="chartOptions"
+      />
+    </div>
+  </div>
 
 </template>
 
@@ -31,43 +31,39 @@ export default {
   },
   data() {
     return{
+      test: [],
       fetchedStock: {},
 
-
-      // multi-lines data sample:
-      // chartData: [
-      // ['Date',      'AAPL', 'MSFT', 'IBM'],
-      // ['2019-01-01', 1000,  400,    200],
-      // ['2019-01-02', 1170,  460,    250],
-      // ['2019-01-03', 660,   1120,   300],
-      // ['2019-01-04', 1030,  540,    350]],
-
-      // single-line data sample:
-      chartData: [
-      ['Date',      'AAPL'],
-      ['2019-02-28', 112.0300],
-      ['2019-01-31', 104.4300],
-      ['2018-12-31', 101.57],
-      ['2018-11-30', 110.89]],
-    selectedChartType: 'LineChart',
-    chartTypes : ["ColumnChart", "PieChart", "BarChart", "LineChart", "AreaChart", "ScatterChart"],
-    chartOptions: {
-      title: '',
-      width: 1000,
-      height: 400,
-      is3D: true
-    },
-  }},
-  mounted(){
-    this.getStocks()
-  },
-  methods: {
-    getStocks(){
-      eventBus.$on('fetch-stock', (stock) => {
-        this.fetchedStock = stock;
-        this.chartOptions.title = Object.keys(this.fetchedStock)[1];
-      });
-    },
-  },
-}
-</script>
+      chartData: [['Date','AAPL']],
+        selectedChartType: 'LineChart',
+        chartTypes : ["ColumnChart", "PieChart", "BarChart", "LineChart", "AreaChart", "ScatterChart"],
+        chartOptions: {
+          title: '',
+          width: 1000,
+          height: 400,
+          is3D: true
+        },
+      }},
+      mounted(){
+        this.getStocks()
+      },
+      methods: {
+        getStocks(){
+          eventBus.$on('fetch-stock', (stock) => {
+            this.fetchedStock = stock;
+            this.chartOptions.title = Object.keys(this.fetchedStock)[1];
+            this.getChartData()
+          });
+        },
+        getChartData(){
+          let timeKey = Object.keys(this.fetchedStock)[1]
+          for (let chunk in this.fetchedStock[timeKey]){
+            let label=chunk
+            let dollarValue = this.fetchedStock[timeKey][chunk]['4. close']
+            let element=[label, dollarValue]
+            this.chartData.push(element)
+          }
+        },
+      },
+    }
+    </script>
