f9dc63e346b4e849fd0d6d4c46bbc5ca19e639cf
John Page <jdehpage@gmail.com>
2019-03-02 14:18:57 +0000

    1st Run 2nd Share into Chart Data

--- client/src/components/Graph.vue
+++ client/src/components/Graph.vue
@@ -21,7 +21,7 @@
 </template>
 
 <script>
-import { eventBus } from '../main.js'
+import { eventBus } from '../main.js';
 import { GChart } from 'vue-google-charts';
 
 export default {
@@ -34,19 +34,15 @@ export default {
 
       test: [],
       fetchedStock: {},
-      chartData: [     ['Date']    ],
-      //
+      //   chartData: [
       // ['Date', 'AAPL', 'TSLA'],
-      //
-      // ["2019-03-02", 1000],
+      // ["2019-03-02", 1, 1000],
       // ["2019-03-01", 1000, 900],
-      // ["2019-02-28", 1170],
-      // ["2019-02-27", 660],
-      // ["2019-02-26", 1030]
-
-
-
-
+      // ["2019-02-28", 1170, 890],
+      // ["2019-02-27", 660, 910],
+      // ["2019-02-26", 1030, 780]
+      // ],
+      chartData: [['Date']],
       selectedChartType: 'LineChart',
       chartTypes : ["ColumnChart", "PieChart", "BarChart", "LineChart", "AreaChart", "ScatterChart"],
       chartOptions: {
@@ -65,30 +61,44 @@ export default {
       eventBus.$on('fetch-stock', (stock) => {
         this.fetchedStock = stock;
         this.chartOptions.title = Object.keys(this.fetchedStock)[1];
-
-
-
         this.chartData[0].push(this.fetchedStock["Meta Data"]["2. Symbol"]);
-
-        this.getChartData()
+        debugger;
+        if (this.chartData[0].length > 2) {
+          this.getMultipleChartData();
+        }
+        else {
+          this.getChartData();
+        };
       });
     },
     getChartData(){
-      let timeKey = Object.keys(this.fetchedStock)[1]
-      let arrayStore = []
+      let timeKey = Object.keys(this.fetchedStock)[1];
+      let arrayStore = [];
       for (let chunk in this.fetchedStock[timeKey]){
-        let label=chunk
-        let dollarValue = Number(this.fetchedStock[timeKey][chunk]['4. close'])
-        let element=[label, dollarValue]
-        arrayStore.push(element)
-      }
-      let array = arrayStore.reverse()
-      for (let thing of array) {
-        this.chartData.push(thing)
-      }
-
-      // this.chartData.push(arrayStore.reverse())
+        let label=chunk;
+        let dollarValue = Number(this.fetchedStock[timeKey][chunk]['4. close']);
+        let element=[label, dollarValue];
+        arrayStore.push(element);
+      };
+      let reversedArray = arrayStore.reverse();
+      for (let closingVal of reversedArray) {
+        this.chartData.push(closingVal);
+      };
+    },
+    getMultipleChartData(){
+      let timeKey = Object.keys(this.fetchedStock)[1];
+      let arrayStore = [];
+      for (let chunk in this.fetchedStock[timeKey]){
+        let dollarValue = Number(this.fetchedStock[timeKey][chunk]['4. close']);
+        arrayStore.push(dollarValue);
+      };
+      let reversedArray = arrayStore.reverse();
+      for (var i = 0; i < this.chartData.length; i++) {
+        if (i > 0) {
+          this.chartData[i].push(reversedArray[i - 1]);
+        };
+      };
     },
   },
-}
+};
 </script>
