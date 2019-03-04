ea3f997a5023730bdb2399af0fbcff49a44e2992
ianbone1 <ianbone1@googlemail.com>
2019-03-04 15:43:11 +0000

    SharePrice tracking now added to Favourites

--- client/src/components/Favourites.vue
+++ client/src/components/Favourites.vue
@@ -7,24 +7,32 @@
         <th>Company</th>
         <th>Purchase Date</th>
         <th>Qty</th>
-        <th>Bought Price</th>
-        <th>Current Price</th>
-        <td>Delta</td>
-        <td>Value</td>
+        <th>Bought Price $</th>
+        <th>Current Price $</th>
+        <th>Profit/Loss $</th>
+        <th>Value $</th>
         <th>Delete</th>
       </tr>
-      <tr v-for='(fav,i) in favourites' v-bind:key="fav">
+      <tr v-for='(fav,i) in favourites' v-bind:key="fav._id">
         <td><input v-on:change="updateDisplay(fav)" class="checkbox" type="checkbox" v-bind:key="i" :id="fav._id" :name="fav._id" :checked="fav.display"/></td>
         <td>{{fav.symbol}}</td>
         <td>{{fav.name}}</td>
         <td><input v-on:change="updateDate(fav)" type="date"  name="fav_date"     :id="fav._id" v-model="fav.purchase_date" :max="todayDate"/>  </td>
         <td><input v-on:change="updateQty(fav)" type="number" name="fav_quantity" :id="fav._id" v-model="fav.qty" min="0" /></td>
-        <td>{{fav.bought_price}}</td>
+        <td><input v-on:change="updateBoughtPrice(fav)" type="number" name="fav_boughtPrice" :id="fav._id" v-model="fav.bought_price" min="0" /></td>
         <td>{{fav.latest_price}}</td>
-        <td>delta</td>
-        <td>value</td>
+        <td>{{((fav.qty * fav.latest_price)-(fav.qty * fav.bought_price)).toFixed(2)}}</td>
+        <td>{{(fav.qty*fav.latest_price).toFixed(2)}}</td>
         <td><button v-on:click="deleteFavourite(fav._id)">X</button></td>
       </tr>
+      <tr>
+        <td></td><td></td><td></td><td></td><td></td><td></td>
+        <td>Totals:</td>
+        <td>{{totalDelta}}</td>
+        <td>{{totalValue}}</td>
+        <td></td>
+      </tr>
+
     </table>
   </div>
 
@@ -40,6 +48,19 @@ export default {
     return{
       favourites: [],
       todayDate: ""
+    }
+  },
+
+  computed:{
+    totalDelta: function(){
+      let delta = 0;
+      this.favourites.forEach((fav) => delta+=((fav.qty * fav.latest_price)-(fav.qty * fav.bought_price)));
+      return delta.toFixed(2);
+    },
+    totalValue: function(){
+      let total = 0;
+      this.favourites.forEach((fav) => total+=(fav.qty * fav.latest_price));
+      return total.toFixed(2);
     }
   },
 
@@ -60,11 +81,7 @@ export default {
     updateDisplay(element){
       //update the database
       let updatedRecord={
-        "symbol": element.symbol,
-        "name":element.name,
-        "qty":element.qty,
         "display": !element.display,
-        "purchase_date": element.purchase_date
       };
       fetch('http://localhost:3000/api/shares/'+ element._id, {
         method: 'PUT',
@@ -80,13 +97,8 @@ export default {
       updateQty(element){
         //update the database
         let index = this.favourites.findIndex(f => f._id == element._id);
-        let oldRecord = this.favourites[index]
         let updatedRecord={
-          "symbol": oldRecord.symbol,
-          "name": oldRecord.name,
-          "qty": element.qty,
-          "display": oldRecord.display,
-          "purchase_date": oldRecord.purchase_date
+          "qty": Number(element.qty),
         };
         fetch('http://localhost:3000/api/shares/'+ element._id, {
           method: 'PUT',
@@ -98,15 +110,26 @@ export default {
           this.emitDisplayFavourites();
         },
 
+        updateBoughtPrice(element){
+          //update the database
+          let index = this.favourites.findIndex(f => f._id == element._id);
+          let updatedRecord={
+            "bought_price": Number(element.bought_price)
+          };
+          fetch('http://localhost:3000/api/shares/'+ element._id, {
+            method: 'PUT',
+            body: JSON.stringify(updatedRecord),
+            headers: { 'Content-Type': 'application/json'}})
+            //update the local favourites object
+            this.favourites[index].qty=element.qty;
+            //fire the event that something has changed.
+            this.emitDisplayFavourites();
+          },
+
         updateDate(element){
           //update the database
           let index = this.favourites.findIndex(f => f._id == element._id);
-          let oldRecord = this.favourites[index]
           let updatedRecord={
-            "symbol": oldRecord.symbol,
-            "name": oldRecord.name,
-            "qty": oldRecord.qty,
-            "display": oldRecord.display,
             "purchase_date": element.purchase_date
           };
           fetch('http://localhost:3000/api/shares/'+ element._id, {
@@ -165,9 +188,9 @@ export default {
             this.favourites.forEach((fav, index) => {
               if (fav.symbol==newPrice.symbol){
                 if (fav.bought_price==null){
-                  this.favourites[index]["bought_price"]=newPrice.value;
+                  this.favourites[index]["bought_price"]=Number(newPrice.value);
                 }
-                this.favourites[index]["latest_price"]=newPrice.value;
+                this.favourites[index]["latest_price"]=Number(newPrice.value);
                 let updatedRecord={
                   "bought_price": this.favourites[index].bought_price,
                   "latest_price": this.favourites[index].latest_price
