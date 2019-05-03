ea1e9f5df4dc2e09be58d2badb8179e43950ac07
Ewan88 <baileyewan@gmail.com>
2019-05-03 12:38:09 +0100

    changes server path to use Mongo Atlas

--- server/server.js
+++ server/server.js
@@ -1,27 +1,36 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const parser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const createRouter = require('./helpers/create_router.js');
 const cors = require('cors');
+const publicPath = path.join(__dirname, '../client/public');
 
+app.use(express.static(publicPath));
 app.use(parser.json());
 app.use(cors());
 
-MongoClient.connect('mongodb://localhost:27017')
-  .then((client) => {
-    const db = client.db('sharetracker');
-    const bookingsCollection = db.collection('shares');
-    const bookingsRouter = createRouter(bookingsCollection);
+let url = 'mongodb+srv://User:Password@cluster0-y4ecu.mongodb.net/test?retryWrites=true' || 'mongodb://localhost:27017';
+let port = process.env.PORT || 3000;
 
-    const symbolsCollection = db.collection('symbols');
-    const symbolsRouter = createRouter(symbolsCollection)
+MongoClient.connect(url, (err, client) => {
+  if (err){
+    console.log(err);
+  }
 
-    app.use('/api/shares', bookingsRouter);
-    app.use('/api/symbols', symbolsRouter);
+  const db = client.db('sharetracker');
+  const bookingsCollection = db.collection('shares');
+  const bookingsRouter = createRouter(bookingsCollection);
+
+  const symbolsCollection = db.collection('symbols');
+  const symbolsRouter = createRouter(symbolsCollection)
+
+  app.use('/api/shares', bookingsRouter);
+  app.use('/api/symbols', symbolsRouter);
+
+  app.listen(port, function () {
+    console.log('Listening on port ' + port);
   })
-  .catch(console.err);
 
-app.listen(3000, function () {
-  console.log(`Listening on port ${ this.address().port }`);
 });
